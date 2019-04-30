3d17265a5997e7f90ac402944145528753c844aa
Henrydlf <36404566+Henrydlf@users.noreply.github.com>
2019-04-30 15:47:14 +0200

    Update interface.php

--- interface.php
+++ interface.php
@@ -37,16 +37,25 @@
 			if(!$resultat)
 				echo "L'identifiant ou le mot de passe est erroné. <br>";
 			else{
-				echo "Bonjour " .$this->user->getID(). ". <br>";
+				session_start();
+				$this->user->setId($resultat['Identifiant']);
+				$this->user->setEmail($resultat['Mail']);
+				$this->user->setName($resultat['Nom']);
+				$this->user->setSurname($resultat['Prenom']);
+				$this->user->setType($resultat['Type']);
+				$this->user->setPassword($resultat['Mdp']);
+				echo "Vous êtes connecté !<br><br>";
 			}
 		}
 
 		public function afficherPanier()
 		{
-			if($this->user->getType() = 'Acheteur')
+			if($this->user->getType() == 'Acheteur')
 				echo "Votre panier est disponible <br><br>";
-			elseif($this->user->getType() = 'Vendeur')
+			elseif($this->user->getType() == 'Vendeur')
 				echo "Vous etes un vendeur";
+			else
+				echo "Pas de type dispo";
 		}
 	}
 ?>
