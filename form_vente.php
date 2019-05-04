64a14532acbcb0fcd9810a7d4b4a0d1cf339cec3
Henrydlf <36404566+Henrydlf@users.noreply.github.com>
2019-05-04 17:32:16 +0200

    uyfu

--- form_vente.php
+++ form_vente.php
@@ -89,133 +89,34 @@ session_start();
 		  <h4>Categories de vente</h4>
 		  <ul class="nav nav-pills nav-stacked">
 		  	<li class="active"><a href="#section2">Livres</a></li>
-		    <li><a href="form_vente.php">Livres</a></li>
-		    <li><a href="form_vente.php">Vetements</a></li>
-		    <li><a href="form_vente.php">Musique</a></li>
-		    <li><a href="form_vente.php">Sports et Loisirs</a></li>
+        <li><a href="vente_vetements.php">Vetements</a></li>
+		    <li><a href="vente_musique.php">Musique</a></li>
+		    <li><a href="vente_sel.php">Sports et Loisirs</a></li>
 		  </ul><br>
 		</div>
+    <h2 style="margin-left:275px">Mettre en vente un livre</h2>
+
+    <div style="margin-left: 300px; margin-top: 25px">
+      <form action="init_vente.php" method="post">
+        <div class="input-group form-group">
+          <input type="text" class="form-control" name="name" placeholder="Nom du produit">  
+        </div>
+        <div class="input-group form-group">
+          <input type="text" class="form-control" name="prix" placeholder="Prix">
+        </div>
+        <div class="input-group form-group">
+          <input type="text" class="form-control" name="image" placeholder="Image">  
+        </div>
+        <div class="input-group form-group">
+          <input style="display: none" type="text" class="form-control" name="categorie" value="livre">  
+        </div>
+        <div class="form-group">
+            <input type="submit" value="Vendre" class="btn float-right login_btn">
+        </div>
+      </form>
+    </div>
 	</div>
 </div>
 
-
-
-
-
-	<h2>Mettre en vente</h2>
-
-	<form action="init_vente.php" method="post">
-		<table>
-			<tr>
-				<td>Categorie:</td>
-				<!--<td><input type="button" value="Livres" onclick="venteLivres()"></td>
-				<td><input type="button" value="Vetements" onclick="venteVetements()"></td>
-				<td><input type="button" value="Musique" onclick="venteMusique()"></td>
-				<td><input type="button" value="Sports et Loisirs" onclick="venteSL()"></td>-->
-				<td>
-					<select id="cat" name="taille" size="1" onchange="det_cat()">
-						<option>Selectionner une categorie</option>
-						<option>Livres</option>
-						<option>Musique</option>
-						<option>Vetements</option>
-						<option>Sports et Loisirs</option>
-					</select>
-				</td>
-			</tr>
-		</table>
-	</form>
-
-	<div class id= "div_section">
-    <p id="acceuil"></p>
-
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.js"></script>
-	<script>
-		function venteVetements(){
-			var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-            	if (this.readyState == 4 && this.status == 200) {
-              		document.getElementById("acceuil").innerHTML = this.responseText;
-            	}
-            };
-            <?php
-  				//$categorie = "Vetements";
-			?>
-            xhttp.open("GET", "parametre_vetements.php", true);                
-            xhttp.send();
-		}
-		function venteAutres(){
-			var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-            	if (this.readyState == 4 && this.status == 200) {
-              		document.getElementById("acceuil").innerHTML = this.responseText;
-            	}
-            };
-            xhttp.open("GET", "parametre_autres.php", true);                
-            xhttp.send();
-		}
-		/*function venteMusique(){
-			var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-            	if (this.readyState == 4 && this.status == 200) {
-              		document.getElementById("acceuil").innerHTML = this.responseText;
-            	}
-            };
-            <?php
-  				//$categorie = "Musique";
-			?>
-            xhttp.open("GET", "parametre_autres.php", true);                
-            xhttp.send();
-		}
-		function venteSL(){
-			var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-            	if (this.readyState == 4 && this.status == 200) {
-              		document.getElementById("acceuil").innerHTML = this.responseText;
-            	}
-            };
-            <?php
-  				//$categorie = "Sports et Loisirs";
-			?>
-            xhttp.open("GET", "parametre_autres.php", true);                
-            xhttp.send();
-		}
-		function venteLivres(){
-			var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-            	if (this.readyState == 4 && this.status == 200) {
-              		document.getElementById("acceuil").innerHTML = this.responseText;
-            	}
-            };
-            <?php
-  				//$categorie = "Livres";
-			?>
-            xhttp.open("GET", "parametre_autres.php", true);                
-            xhttp.send();
-		}*/
-		function det_cat(){
-			var cat = document.getElementById("cat").value; 
-			if(cat == "Livres")
-			{
-				<?php $categorie = "Livres";?>
-				venteAutres();
-			}
-			if(cat == "Musique")
-			{
-				<?php $categorie = "Musique";?>
-				venteAutres();
-			}
-			if(cat == "Vetements")
-			{
-				<?php $categorie = "Vetements";?>
-				venteVetements();
-			}
-			if(cat == "Sports et Loisirs")
-			{
-				<?php $categorie = "Sports et Loisirs";?>
-				venteAutres();
-			}
-				
-		}
-	</script>
 </body>
 </html>
