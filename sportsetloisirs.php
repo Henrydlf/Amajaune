84753db4e16d407a028d5e3da788948ec9c880e2
Henrydlf <36404566+Henrydlf@users.noreply.github.com>
2019-05-05 15:04:37 +0200

    maj

--- sportsetloisirs.php
+++ sportsetloisirs.php
@@ -106,7 +106,10 @@ try{
             <div class="panel panel-primary">
              <!-- <div class="panel-heading"><?php echo $donnees ['Nom'];?></div> -->
               <div class="panel-body"><img src="images_main/<?php echo $donnees ['Image']; ?>" class="img-responsive" style="width:50%" alt="Image"></div>
-              <div class="panel-footer"> <?php echo $donnees ['Nom'];?> - <?php echo $donnees ['Prix'];?>€ </div>
+              <div class="panel-footer">
+                <div><?php echo $donnees ['Nom'];?> - <?php echo $donnees ['Prix'];?>€</div>
+                <div class="panel-footer"><button class="btn btn-block btn-success">Ajouter au panier</button></div>
+              </div>
             </div>
           </div>
 
